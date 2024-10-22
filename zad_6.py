9bcf31cc6743ff22008fd478bb65f578847cd051
marcinserwata <serwatamarcin@gmail.com>
2024-10-22 12:53:41 +0200

    Zadania 1-8

---
+++ zad_6.py
@@ -0,0 +1,15 @@
+# Stworzyć funkcję, która przyjmuje 2 argumenty typu list i zwraca wynik typu
+# list . Funkcja ma za zadanie złączyć przekazane listy w jedną, usunąć
+# duplikaty, każdy element podnieść do potęgi 3 stopnia, a następnie zwrócić
+# powstałą listę.
+
+def merge_lists(list1, list2):
+    merged_list = list1 + list2
+    merged_list = list(set(merged_list))
+    merged_list = [x ** 3 for x in merged_list]
+    return merged_list
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+result = merge_lists(list1, list2)
+print(result)
